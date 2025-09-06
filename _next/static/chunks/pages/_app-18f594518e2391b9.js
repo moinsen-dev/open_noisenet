bed74c733bc13e398d5e93f998e6417b166df1e9
udiedrichsen <udiedrichsen@users.noreply.github.com>
2025-09-06 10:08:30 +0000

    deploy: 9d03161f735e3e09729410bbfb2e80baa81d1187

---
+++ _next/static/chunks/pages/_app-18f594518e2391b9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{1289:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7335)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(1289),n(7151))),_N_E=_.O()}]);
